1ba34dcc34c9fc7094cfdc59532c7955f3937137
amywisegarver <55897662+amywisegarver@users.noreply.github.com>
2019-12-13 15:17:00 -0800

    Add files via upload

---
+++ clock2.js
@@ -0,0 +1,36 @@
+  
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+    
+    
+ if (s < 5) {
+  document.getElementById("txt").style.color = "white";
+  document.body.style.backgroundImage = "url('https://media.giphy.com/media/3Hrq0z3lc9vKMGcpv3/giphy.gif')";
+} else if (s < 5) {
+  document.getElementById("txt").style.color = "pink";
+  document.body.style.backgroundImage = "url('https://media.giphy.com/media/4qjP6rIloYJEs/giphy.gif')";
+} else {
+   document.getElementById("txt").style.color = "lightBlue";
+  document.body.style.backgroundImage = "url('https://media.giphy.com/media/fdXJLBMujaN1VE2U6c/giphy.gif')";
+}    
+    
+
+}
+
+
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i; 
+    
+    
+}
